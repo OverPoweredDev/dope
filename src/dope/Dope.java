3cab61499c7573bd040eeafa8cc934cfe4e84eb4
OverPoweredDev <omkar.prabhune19@vit.edu>
2021-04-18 12:20:09 +0530

    finished Parsing

--- src/dope/Dope.java
+++ src/dope/Dope.java
@@ -14,7 +14,7 @@ public class Dope {
 
     public static void main(String[] args) throws IOException {
         if (args.length > 1) {
-            System.out.println("Usage: jlox [script]");
+            System.out.println("Usage: dope [script]");
             System.exit(64);
         } else if (args.length == 1) {
             runFile(args[0]);
